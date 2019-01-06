7508e5de4b8eef18c3aca4f11eafecfd8f33c939
xmilygit <xmilygame@163.com>
2019-01-07 01:13:41 +0800

    改进axios的默认地址设置方法

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
-axios.defaults.baseURL="http://192.168.123.151:3000";//"http://188.188.3.2:8008";//"http://188.188.1.15:3000";//'http://localhost:3000/'
+const host=window.location.host
+axios.defaults.baseURL="http://"+host//"http://192.168.123.151:3000";//"http://188.188.3.2:8008";//"http://188.188.1.15:3000";//'http://localhost:3000/'
 Vue.prototype.$ajax=axios
 
 
