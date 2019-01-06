1635d9262b40a2896c92d290e6b9e90cca536756
xmilygit <xmilygame@163.com>
2019-01-06 17:27:51 +0800

    axios路径修改

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
-axios.defaults.baseURL="http://188.188.3.2:8008";//"http://192.168.123.151:3000";//"http://188.188.1.15:3000";//'http://localhost:3000/'
+axios.defaults.baseURL="http://192.168.123.151:3000";//"http://188.188.3.2:8008";//"http://188.188.1.15:3000";//'http://localhost:3000/'
 Vue.prototype.$ajax=axios
 
 
