7e5c35e349591652c5b1f02c941f3413b801dab3
xmilygit <xmilygame@163.com>
2020-07-28 21:52:54 +0800

    nginx 做服务器

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
 const host=window.location.host
-axios.defaults.baseURL="http://192.168.123.151:3000";//"http://"+host//"http://188.188.3.2:8008";//"http://188.188.1.15:3000";//'http://localhost:3000/'
+axios.defaults.baseURL="http://188.188.3.2:8089/api";//"http://"+host//"http://188.188.3.2:8008";//"http://188.188.1.15:3000";//'http://localhost:3000/'
 Vue.prototype.$ajax=axios
 
 
